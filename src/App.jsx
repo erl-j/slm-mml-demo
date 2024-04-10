49972a41292bce1b759bc82c8e69950f540adcd9
erl-j <nicolasjonason@gmail.com>
2024-04-10 14:35:12 +0200

    css stuff

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,74 @@ import MIDIPlayer from './MIDIPlayer'
 const App = ({}) => {
   // const [count, setCount] = useState(0)
 
-  const index = 90;
+  const n_samples = 100;
 
-  const task = "generate"
+  const tasks = ["infilling_high","infilling_low","constrained_generation", "infilling_start","infilling_end", "generate"]
+  const temperatures = ["0.85","0.9","1.0"]
+  const [index, setIndex] = useState(0)
+  const [task,setTask] = useState("infilling_high")
+  const [temperature, setTemperature] = useState("1.0")
+
 
   const natural_example = {
     "path": `public/artefacts/eval_cropped_midi/fad_test/natural/nr_${index}_cropped.mid`,
   }
 
   const mlm_example = {
-    "path": `public/artefacts/eval_cropped_midi/fad_test/${task}/_mlm_t=1.0/nr_${index}_cropped.mid`,
+    "path": `public/artefacts/eval_cropped_midi/fad_test/${task}/${task ==="constrained_generation" ? "" : "_"}mlm_t=${temperature}/nr_${index}_cropped.mid`,
   }
   const slm_example = {
-    "path": `public/artefacts/eval_cropped_midi/fad_test/${task}/_slm_t=1.0/nr_${index}_cropped.mid`,
+    "path": `public/artefacts/eval_cropped_midi/fad_test/${task}/${task ==="constrained_generation" ? "" : "_"}slm_t=${temperature}/nr_${index}_cropped.mid`,
   }
   const examples = [natural_example,mlm_example, slm_example]
 
   const [currentFile, setCurrentFile] = useState(null)
 
   return (
+    <div>
+      <h1>Sample nr {index}/{n_samples}</h1>
+    <div>
+      
+      <div>
+      <button onClick={() => setIndex(
+        // mod n_samples
+        (index - 1 + n_samples) % n_samples
+      )}>Previous</button>
+      <button 
+      onClick={() => setIndex(
+        // mod n_samples
+        (index + 1) % n_samples
+
+      )}>Next</button>
+      <button 
+      onClick={() => {
+        const random = Math.floor(Math.random() * n_samples-1)
+        // mod n_samples
+        setIndex((random + index + 1) % n_samples)
+        }
+      }>Random</button>
+      <div>
+        </div>
+        {tasks.map((t) =>
+          <button
+          key={t}
+          style={t === task ? {backgroundColor:"lightblue"} : {backgroundColor:"white"}}
+           onClick={() => setTask(t)}>{t}</button>
+        )}
+      </div>
+      <div>
+        {temperatures.map((t) =>
+          <button 
+          key={t}
+          style={t === temperature ? {backgroundColor:"lightblue"} : {backgroundColor:"white"}}
+          onClick={() => setTemperature(t)}>{t}</button>
+        )}
+      </div>
+      
+    </div>
     <div style={{display:"flex", flexDirection:"row", border:"1px solid black", width:"100%"}}>
+      
+
       {examples.map((ex) =>
         <div key={ex.path}
           onClick={() => {
@@ -36,7 +84,9 @@ const App = ({}) => {
             }
         }}
         >
-          {/* <h3>{ex.path}</h3> */}
+          <span
+          style={{fontSize:"2px"}}
+          >{ex.path}</span>
           <MIDIPlayer
             src={ex.path}
             isPlaying={currentFile === ex.path}
@@ -44,6 +94,8 @@ const App = ({}) => {
         </div>
       )}
     </div>
+    </div>
+
   )
 
 
