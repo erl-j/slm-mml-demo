175c350501a98f7329a3c4c71e738e3396a39785
erl-j <nicolasjonason@gmail.com>
2024-05-02 06:40:59 +0200

    add this

--- src/App.jsx
+++ src/App.jsx
@@ -25,54 +25,54 @@ const App = ({ }) => {
     "generate": {
       "title": "Unconditional generation",
       "description": "Generate a loop from scratch without any conditioning/constraints whatsoever. Note that example nr 1 contains no notes.",
-      "parameters": "top-p=0.75, T=200 (Number of forward passes)"
+      "parameters": "top-p=0.75, T=200."
     },
     "generate_w_constraints": {
       "title": "Conditional generation",
       "description": "Generate a loop with constraints. The constraint used here consists of restricting pitch to the C major pitch-set, using only drums, piano, guitar, bass with at least 10 notes per instrument, and between 50 and 250 notes in total.",
 
-      "parameters": "top-p=0.99, T=300 (Number of forward passes)"
+      "parameters": "top-p=0.99, T=300."
     },
     "variation": {
       "title": "Variation",
       "description": "Generate a variation of the loop. We do this by taking our source loop, turning it into a one-hot-like probability distribution, and mixing it with a uniform prior.",
 
-      "parameters": "top-p=0.75, T=300 (Number of forward passes)"
+      "parameters": "top-p=0.75, T=300."
     },
     "infilling_high": {
       "title": "Infill upper half",
       "description": "Regenerate the upper half of the loop's pitch range, drums are kept the same.",
-      "parameters": "top-p=0.75, T=300 (Number of forward passes)"
+      "parameters": "top-p=0.75, T=300."
     },
     "infilling_low": {
       "title": "Infill lower half",
       "description": "Regenerate the lower half of the loop's pitch range, drums are kept the same.",
-      "parameters": "top-p=0.5, T=200 (Number of forward passes)"
+      "parameters": "top-p=0.5, T=200."
     },
     "infilling_box_middle": {
       "title": "Infill middle box",
       "description": "Regenerate upper and lower half of the loop's pitch range for bars 2 and 3, drums are kept the same.",
-      "parameters": "top-p=0.75, T=300 (Number of forward passes)"
+      "parameters": "top-p=0.75, T=300."
     },
     "infilling_middle": {
       "title": "Infill middle",
       "description": "Regenerate bars 2 and 3 of the loop.",
-      "parameters": "top-p=0.75, T=300 (Number of forward passes)"
+      "parameters": "top-p=0.75, T=300."
     },
     "replace_bass": {
       "title": "Replace bass",
       "description": "Replace the bass of the loop.",
-      "parameters": "top-p=0.75, T=300 (Number of forward passes)"
+      "parameters": "top-p=0.75, T=300."
     },
     "replace_drums": {
       "title": "Replace drums",
       "description": "Replace the drums of the loop.",
-      "parameters": "top-p=0.75, T=300 (Number of forward passes)"
+      "parameters": "top-p=0.75, T=300."
     },
     "pitch_set": {
       "title": "Replace pitch",
       "description": "Regenerate all the pitches of the loop. In these examples, the pitches are restricted to the set of pitches in the source loop.",
-      "parameters": "top-p=0.85, T=300 (Number of forward passes)"
+      "parameters": "top-p=0.85, T=300."
     },
   }
 
@@ -114,7 +114,7 @@ const App = ({ }) => {
   }, [task, index])
 
   return (
-    <div style={{ width: "100vw", margin: 16 }}>
+    <div style={{ margin: 16 }}>
       <h3> Demo for ICCC Short Paper Submission 172: SYMPLEX: Fast, Flexible and Controllable Symbolic Music Generation using
         Simplex Diffusion</h3>
       <p>This website was tested on Chrome (Version 124.0.6367.78).
@@ -144,7 +144,7 @@ const App = ({ }) => {
           <div>
             
             <p><b>Task description: </b>{taskMeta[task].description}</p>
-            <p>Parameters used: {taskMeta[task].parameters}</p>
+            <p><b>Parameters used: </b>{taskMeta[task].parameters}</p>
 
           </div>
           <div>
@@ -177,11 +177,9 @@ const App = ({ }) => {
 
       </div>
       <div style={{ display: "flex", flexDirection: "row", width: "100%", justifyContent: "space-evenly" }}>
-
-
         {examples.map((ex) =>
           <div key={ex.path}
-            style={{ flex: 1, margin: "4px" }}
+            style={{ flex: 1, margin: "0px" }}
             onClick={() => {
               if (currentFile === ex.path) {
                 setCurrentFile(null)
