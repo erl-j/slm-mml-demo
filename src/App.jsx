266f1b6d3dddc3b59268c68eb0d214b63e16abb1
erl-j <nicolasjonason@gmail.com>
2024-06-19 11:19:30 +0200

    start working on multiinstrument support

--- src/App.jsx
+++ src/App.jsx
@@ -5,180 +5,104 @@ import MIDIPlayer from './MIDIPlayer'
 const App = ({ }) => {
   // const [count, setCount] = useState(0)
 
-  const n_samples = 5;
+  const n_samples = 100;
 
   const tasks = [
-    // "constrained_generation",
-    "infilling_middle",
-    "infilling_high",
-    "infilling_low",
-    "infilling_box_middle",
-    "replace_bass",
-    "replace_drums",
+    // "infilling_high", 
+    "infilling_start", 
+    "infilling_end", 
+    "infilling_low", 
+
+    "infilling_high_patched",
     "pitch_set",
-    "variation",
-    "generate",
-    "generate_w_constraints",
+    // "pitch_onset_set",
+    // "onset_set",
+    // "infilling_box_end",
+    "infilling_box_middle",
+    // "infilling_drums",
+    // "infilling_harmonic",
+    "constrained_generation", 
+
+    // "generate", 
   ]
-
-  const taskMeta = {
-    "generate": {
-      "title": "Unconditional generation",
-      "description": "Generate a loop from scratch without any conditioning/constraints whatsoever. Note that example nr 1 contains no notes.",
-      "parameters": "top-p=0.75, T=200."
-    },
-    "generate_w_constraints": {
-      "title": "Conditional generation",
-      "description": "Generate a loop with constraints. The constraint used here consists of restricting pitch to the C major pitch-set, using only drums, piano, guitar, bass with at least 10 notes per instrument, and between 50 and 250 notes in total.",
-
-      "parameters": "top-p=0.99, T=300."
-    },
-    "variation": {
-      "title": "Variation",
-      "description": "Generate a variation of the loop. We do this by taking our source loop, turning it into a one-hot-like probability distribution, and mixing it with a uniform prior.",
-
-      "parameters": "top-p=0.75, T=300."
-    },
-    "infilling_high": {
-      "title": "Infill upper half",
-      "description": "Regenerate the upper half of the loop's pitch range, drums are kept the same.",
-      "parameters": "top-p=0.75, T=300."
-    },
-    "infilling_low": {
-      "title": "Infill lower half",
-      "description": "Regenerate the lower half of the loop's pitch range, drums are kept the same.",
-      "parameters": "top-p=0.5, T=200."
-    },
-    "infilling_box_middle": {
-      "title": "Infill middle box",
-      "description": "Regenerate upper and lower half of the loop's pitch range for bars 2 and 3, drums are kept the same.",
-      "parameters": "top-p=0.75, T=300."
-    },
-    "infilling_middle": {
-      "title": "Infill middle",
-      "description": "Regenerate bars 2 and 3 of the loop.",
-      "parameters": "top-p=0.75, T=300."
-    },
-    "replace_bass": {
-      "title": "Replace bass",
-      "description": "Replace the bass of the loop.",
-      "parameters": "top-p=0.75, T=300."
-    },
-    "replace_drums": {
-      "title": "Replace drums",
-      "description": "Replace the drums of the loop.",
-      "parameters": "top-p=0.75, T=300."
-    },
-    "pitch_set": {
-      "title": "Replace pitch",
-      "description": "Regenerate all the pitches of the loop. In these examples, the pitches are restricted to the set of pitches in the source loop.",
-      "parameters": "top-p=0.85, T=300."
-    },
-  }
-
-  // const modelsAvailable = [
-  //   "hz_512_",
-  //   "hz_768_"
-  // ]
-
-
+  const temperatures = ["0.85", "0.9","0.95", "1.0"]
   const [index, setIndex] = useState(0)
-  const [task, setTask] = useState("infilling_middle")
-  // const [temperature, setTemperature] = useState("0.9")
+  const [task, setTask] = useState("generate")
+  const [temperature, setTemperature] = useState("0.9")
+
 
   const natural_example = {
-    "path": `simplex_demo_2_website_pp/natural/nr_${index}.mid`,
+    "path": `artefacts/eval_cropped_midi/fad_test_sane/natural/nr_${index}_cropped.mid`,
   }
 
-  const simplex_example = {
-    "path": `simplex_demo_2_website_pp/${task}/nr_${index}.mid`,
+  const mlm_example = {
+    "path": 'artefacts/eval_cropped_midi/fad_test_sane/' + task + '/' + 'mlm_t=' + temperature + '/nr_' + index + '_cropped.mid',
   }
-
-  let examples = []
-  if (task === "generate" || task === "generate_w_constraints") {
-    examples = [simplex_example]
-
+  const slm_example = {
+    "path": 'artefacts/eval_cropped_midi/fad_test_sane/' + task + '/' + 'slm_t=' + temperature + '/nr_' + index + '_cropped.mid',
   }
-  else {
-    examples = [natural_example, simplex_example]
-  }
-
+  const examples = [natural_example, mlm_example, slm_example]
 
   const [currentFile, setCurrentFile] = useState(null)
-
 
   // use effect that when something changes, it sets current file to null
   useEffect(() => {
     // stop all audio on this page
     setCurrentFile(null)
-  }, [task, index])
+  }, [task, temperature, index])
 
   return (
-    <div style={{ margin: 16 }}>
-      <h3>Demo for SYMPLEX: Controllable Symbolic Music Generation using Simplex Diffusion with Vocabulary Priors</h3>
-      <p>This website was tested on Chrome (Version 124.0.6367.78).
-        <br />
-        Play audio by clicking on a piano roll. Stop the audio by clicking the active piano roll again.
-        <br />
-        Note that the colours used to indicate the instruments are not consistent across loops.
-        <br />
-        The inference parameters top-p and T were set on a task by task basis and are displayed below the task descriptions.
-        For each task, the examples were generated in one batch using a fixed random seed.
-      </p>
-
+    <div style={{ width: "100vw" }}>
+      <h1>Sample nr {index}/{n_samples}</h1>
       <div>
 
         <div>
+          <button onClick={() => setIndex(
+            // mod n_samples
+            (index - 1 + n_samples) % n_samples
+          )}>previous sample</button>
+          <button
+            onClick={() => setIndex(
+              // mod n_samples
+              (index + 1) % n_samples
+
+            )}>next sample</button>
+          <button
+            onClick={() => {
+              const random = Math.floor(Math.random() * n_samples - 1)
+              // mod n_samples
+              setIndex((random + index + 1) % n_samples)
+            }
+            }>random sample</button>
+          <div>
+          </div>
           <div>
             <h3>Task:</h3>
             {tasks.map((t) =>
               <button
                 key={t}
                 style={t === task ? { backgroundColor: "lightblue" } : { backgroundColor: "white" }}
-                onClick={() => setTask(t)}>{
-                  taskMeta[t].title
-                }</button>
+                onClick={() => setTask(t)}>{t}</button>
             )}
           </div>
-          <div>
-            
-            <p><b>Task description: </b>{taskMeta[task].description}</p>
-            <p><b>Parameters used: </b>{taskMeta[task].parameters}</p>
-
-          </div>
-          <div>
-            <h4>Example nr {index}/{n_samples}:</h4>
-
-            <button onClick={() => setIndex(
-              // mod n_samples
-              (index - 1 + n_samples) % n_samples
-            )}>previous sample</button>
-            <button
-              onClick={() => setIndex(
-                // mod n_samples
-                (index + 1) % n_samples
-
-              )}>next sample</button>
-            <button
-              onClick={() => {
-                const random = Math.floor(Math.random() * n_samples - 1)
-                // mod n_samples
-                setIndex((random + index + 1) % n_samples)
-              }
-              }>random sample</button>
-          </div>
-
-
         </div>
         <div>
-
+          <h3>Temperature:</h3>
+          {temperatures.map((t) =>
+            <button
+              key={t}
+              style={t === temperature ? { backgroundColor: "lightblue" } : { backgroundColor: "white" }}
+              onClick={() => setTemperature(t)}>{t}</button>
+          )}
         </div>
 
       </div>
-      <div style={{ display: "flex", flexDirection: "row", width: "100%", justifyContent: "space-evenly" }}>
+      <div style={{ display: "flex", flexDirection: "row", width: "100%", justifyContent:"space-evenly" }}>
+
+
         {examples.map((ex) =>
           <div key={ex.path}
-            style={{ flex: 1, margin: "0px" }}
+          style={{flex:1,margin:"4px"}}
             onClick={() => {
               if (currentFile === ex.path) {
                 setCurrentFile(null)
@@ -188,9 +112,8 @@ const App = ({ }) => {
               }
             }}
           >
-            <h3>{ex.path.includes("natural") ? "Source loop" : "Generation result"}</h3>
             <span
-              style={{ color: "white" }}
+              style={{ fontSize: "2px" }}
             >{ex.path}</span>
             <MIDIPlayer
               src={ex.path}
---
+++ src/App2.jsx
@@ -0,0 +1,209 @@
+import { useState, useRef, useEffect } from 'react'
+import './App.css'
+import MIDIPlayer from './MIDIPlayer'
+
+const App = ({ }) => {
+  // const [count, setCount] = useState(0)
+
+  const n_samples = 5;
+
+  const tasks = [
+    // "constrained_generation",
+    "infilling_middle",
+    "infilling_high",
+    "infilling_low",
+    "infilling_box_middle",
+    "replace_bass",
+    "replace_drums",
+    "pitch_set",
+    "variation",
+    "generate",
+    "generate_w_constraints",
+  ]
+
+  const taskMeta = {
+    "generate": {
+      "title": "Unconditional generation",
+      "description": "Generate a loop from scratch without any conditioning/constraints whatsoever. Note that example nr 1 contains no notes.",
+      "parameters": "top-p=0.75, T=200."
+    },
+    "generate_w_constraints": {
+      "title": "Conditional generation",
+      "description": "Generate a loop with constraints. The constraint used here consists of restricting pitch to the C major pitch-set, using only drums, piano, guitar, bass with at least 10 notes per instrument, and between 50 and 250 notes in total.",
+
+      "parameters": "top-p=0.99, T=300."
+    },
+    "variation": {
+      "title": "Variation",
+      "description": "Generate a variation of the loop. We do this by taking our source loop, turning it into a one-hot-like probability distribution, and mixing it with a uniform prior.",
+
+      "parameters": "top-p=0.75, T=300."
+    },
+    "infilling_high": {
+      "title": "Infill upper half",
+      "description": "Regenerate the upper half of the loop's pitch range, drums are kept the same.",
+      "parameters": "top-p=0.75, T=300."
+    },
+    "infilling_low": {
+      "title": "Infill lower half",
+      "description": "Regenerate the lower half of the loop's pitch range, drums are kept the same.",
+      "parameters": "top-p=0.5, T=200."
+    },
+    "infilling_box_middle": {
+      "title": "Infill middle box",
+      "description": "Regenerate upper and lower half of the loop's pitch range for bars 2 and 3, drums are kept the same.",
+      "parameters": "top-p=0.75, T=300."
+    },
+    "infilling_middle": {
+      "title": "Infill middle",
+      "description": "Regenerate bars 2 and 3 of the loop.",
+      "parameters": "top-p=0.75, T=300."
+    },
+    "replace_bass": {
+      "title": "Replace bass",
+      "description": "Replace the bass of the loop.",
+      "parameters": "top-p=0.75, T=300."
+    },
+    "replace_drums": {
+      "title": "Replace drums",
+      "description": "Replace the drums of the loop.",
+      "parameters": "top-p=0.75, T=300."
+    },
+    "pitch_set": {
+      "title": "Replace pitch",
+      "description": "Regenerate all the pitches of the loop. In these examples, the pitches are restricted to the set of pitches in the source loop.",
+      "parameters": "top-p=0.85, T=300."
+    },
+  }
+
+  // const modelsAvailable = [
+  //   "hz_512_",
+  //   "hz_768_"
+  // ]
+
+
+  const [index, setIndex] = useState(0)
+  const [task, setTask] = useState("infilling_middle")
+  // const [temperature, setTemperature] = useState("0.9")
+
+  const natural_example = {
+    "path": `simplex_demo_2_website_pp/natural/nr_${index}.mid`,
+  }
+
+  const simplex_example = {
+    "path": `simplex_demo_2_website_pp/${task}/nr_${index}.mid`,
+  }
+
+  let examples = []
+  if (task === "generate" || task === "generate_w_constraints") {
+    examples = [simplex_example]
+
+  }
+  else {
+    examples = [natural_example, simplex_example]
+  }
+
+
+  const [currentFile, setCurrentFile] = useState(null)
+
+
+  // use effect that when something changes, it sets current file to null
+  useEffect(() => {
+    // stop all audio on this page
+    setCurrentFile(null)
+  }, [task, index])
+
+  return (
+    <div style={{ margin: 16 }}>
+      <h3>Demo for SYMPLEX: Controllable Symbolic Music Generation using Simplex Diffusion with Vocabulary Priors</h3>
+      <p>This website was tested on Chrome (Version 124.0.6367.78).
+        <br />
+        Play audio by clicking on a piano roll. Stop the audio by clicking the active piano roll again.
+        <br />
+        Note that the colours used to indicate the instruments are not consistent across loops.
+        <br />
+        The inference parameters top-p and T were set on a task by task basis and are displayed below the task descriptions.
+        For each task, the examples were generated in one batch using a fixed random seed.
+      </p>
+
+      <div>
+
+        <div>
+          <div>
+            <h3>Task:</h3>
+            {tasks.map((t) =>
+              <button
+                key={t}
+                style={t === task ? { backgroundColor: "lightblue" } : { backgroundColor: "white" }}
+                onClick={() => setTask(t)}>{
+                  taskMeta[t].title
+                }</button>
+            )}
+          </div>
+          <div>
+            
+            <p><b>Task description: </b>{taskMeta[task].description}</p>
+            <p><b>Parameters used: </b>{taskMeta[task].parameters}</p>
+
+          </div>
+          <div>
+            <h4>Example nr {index}/{n_samples}:</h4>
+
+            <button onClick={() => setIndex(
+              // mod n_samples
+              (index - 1 + n_samples) % n_samples
+            )}>previous sample</button>
+            <button
+              onClick={() => setIndex(
+                // mod n_samples
+                (index + 1) % n_samples
+
+              )}>next sample</button>
+            <button
+              onClick={() => {
+                const random = Math.floor(Math.random() * n_samples - 1)
+                // mod n_samples
+                setIndex((random + index + 1) % n_samples)
+              }
+              }>random sample</button>
+          </div>
+
+
+        </div>
+        <div>
+
+        </div>
+
+      </div>
+      <div style={{ display: "flex", flexDirection: "row", width: "100%", justifyContent: "space-evenly" }}>
+        {examples.map((ex) =>
+          <div key={ex.path}
+            style={{ flex: 1, margin: "0px" }}
+            onClick={() => {
+              if (currentFile === ex.path) {
+                setCurrentFile(null)
+              }
+              else {
+                setCurrentFile(ex.path)
+              }
+            }}
+          >
+            <h3>{ex.path.includes("natural") ? "Source loop" : "Generation result"}</h3>
+            <span
+              style={{ color: "white" }}
+            >{ex.path}</span>
+            <MIDIPlayer
+              src={ex.path}
+              isPlaying={currentFile === ex.path}
+            />
+          </div>
+        )}
+      </div>
+    </div>
+
+  )
+
+
+}
+
+export default App
