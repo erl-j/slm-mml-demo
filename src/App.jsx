2abb262191cb94d5f0297cd572f2ad498089eaea
Nicolas J <nicolasjonason@gmail.com>
2025-02-18 15:27:21 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -53,7 +53,7 @@ const App = () => {
         <iframe
           width="100%"
           height="100%"
-          src="https://www.youtube.com/embed/etuF94r-3hM?si=xdDlTvufmzFId278"
+          src="https://www.youtube.com/embed/mCdI9-re40A?si=d5FqrwNLQALaS4B7"
           title="YouTube video player"
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
